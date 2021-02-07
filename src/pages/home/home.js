39f98278e4ffb21964712dc97db00c9012fc685d
okrysiuk <oleksandr.krysiuk@gmail.com>
2021-02-07 17:54:25 +0200

    solved scroll problem

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import LandingSection from "./../../components/landing-section";
 import WhySection from "./../../components/why-section";
 
 const Home = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
       <LandingSection
